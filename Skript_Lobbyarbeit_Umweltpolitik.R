f44318f3daf26efae12ea8f8cd4d5e5cc4b4258c
hdm-ja047 <37869798+hdm-ja047@users.noreply.github.com>
2018-10-24 11:13:24 +0200

    Add files via upload

---
+++ Skript_Lobbyarbeit_Umweltpolitik.R
@@ -0,0 +1,35 @@
+
+library(igraph)
+
+el <- read.csv("https://raw.githubusercontent.com/hdm-gk038/Lobby/master/226305_Projekt_Lobbyarbeit_Edgelist.csv", header=T, as.is=T, quote="", sep = ",")
+nl <- read.csv("https://raw.githubusercontent.com/hdm-gk038/Lobby/master/226305_Projekt_Lobbyarbeit_Nodelist.csv", header=T, as.is=T, quote="", sep = ",")
+
+head(el)
+head(nl)
+
+a <-as.matrix(el)
+g <- graph_from_data_frame(d=el, vertices=nl, directed=T)
+
+V(g)$size <- 6
+weight <- E(g)$weight
+E(g)$width <- weight*1.5
+E(g)$arrow.size <- 0
+
+label <- V(g)$label
+
+colrs <- c("yellowgreen", "deepskyblue3", "darkorange2")
+V(g)$color <- colrs[V(g)$type]
+
+thema <- V(g)[type == "3"]
+thema
+V(g)[thema]$shape = "square"
+
+
+
+
+plot(g, layout=layout_with_fr)
+
+
+
+
+
